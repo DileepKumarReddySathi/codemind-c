aa0a41d3fe28290b401d2d212bc1bfcbbcec1c51
DileepKumarReddySathi <147469601+DileepKumarReddySathi@users.noreply.github.com>
2023-12-19 10:40:17 +0530

    #include<stdio.h> int main() { 	int i,a,sum; 	scanf("%d", &a); 	int arr[a]; 	for(i=0;i<a;i++) 	{ 		scanf("%d", &arr[i]); 	} 	for(i=0;i<a;i=i+2) 	{ 	    sum+=arr[i]; 	} 	printf("%d", sum); }

--- Sum_of_Even_Positions.c
+++ Sum_of_Even_Positions.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int i,a,sum;
-    scanf("%d", &a);
-    int arr[a];
-    for(i=0;i<a;i++)
-    {
-        scanf("%d",&arr[i]);
-    }
-    for(i=0;i<a;i=i+2)
-    {
-    sum=sum+arr[i];
-    }
-    printf("%d", sum);
+	int i,a,sum;
+	scanf("%d", &a);
+	int arr[a];
+	for(i=0;i<a;i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	for(i=0;i<a;i=i+2)
+	{
+	    sum+=arr[i];
+	}
+	printf("%d", sum);
 }
